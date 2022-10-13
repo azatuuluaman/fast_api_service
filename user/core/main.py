f540a3ca46d9b269e7d59669192910f35f291fb7
Shairbekov Bakyt <cyberstalker0726@gmail.com>
2022-10-13 10:46:30 +0600

    feature/add_flake8_add_black

--- user/core/main.py
+++ user/core/main.py
@@ -1,12 +1,9 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI
 
 from user.api.users import router
 
 
 app = FastAPI(openapi_url="/api/v1/users/openapi.json", docs_url="/api/v1/users/docs")
-
-
-
 
 
 app.include_router(router, prefix="/api/v1/users", tags=["users"])
