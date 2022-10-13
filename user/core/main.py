bf08712ac9ce55e770c6704bc75f9c0d7c2532cd
Shairbekov Bakyt <cyberstalker0726@gmail.com>
2022-10-13 11:08:39 +0600

    feature/update_pre-commit-hook

--- user/core/main.py
+++ user/core/main.py
@@ -5,5 +5,4 @@ from user.api.users import router
 
 app = FastAPI(openapi_url="/api/v1/users/openapi.json", docs_url="/api/v1/users/docs")
 
-
 app.include_router(router, prefix="/api/v1/users", tags=["users"])
